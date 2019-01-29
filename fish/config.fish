bbfdf8ce104025fd0e80110a1f8f13729e6fa789
Andreas Pfau <andreas.pfau@sap.com>
2019-01-29 13:55:43 +0100

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -4,8 +4,6 @@ set -x EDITOR (which vim)
 
 set -g fish_user_paths "/usr/local/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/openssl/bin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
 
 alias ls='ls -A'
 
