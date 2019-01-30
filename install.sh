9b147e2269717163724a682b81768eca627ac28c
Andreas Pfau <andreas.pfau@sap.com>
2019-01-30 14:52:57 +0100

    add vimrc

--- install.sh
+++ install.sh
@@ -2,3 +2,7 @@ echo "Load Dotfiles..."
 
 ln -s $(pwd)/fish/config.fish ~/.config/fish/config.fish
 ln -s $(pwd)/fish/functions ~/.config/fish/functions
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+ln -s $(pwd)/vimrc ~/.vimrc
