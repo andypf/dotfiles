38d2108b249be539371703eb1ab597dfbd536da4
Andreas Pfau <andreas.pfau@sap.com>
2019-01-31 09:23:36 +0000

    replace root login shell with fish

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 echo "Load Dotfiles..."
 
+sed -i 's/\(root:.*\/root:\)\/bin\/ash/\1\/usr\/bin\/fish/g' /etc/passwd
+
 ln -s $(pwd)/fish/config.fish ~/.config/fish/config.fish
 ln -s $(pwd)/fish/functions ~/.config/fish/functions
 
