0b473878b43b6ff3be5d8e45097f70b8595348a3
Andreas Pfau <andreas.pfau@sap.com>
2019-02-04 16:13:30 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,15 +2,15 @@ echo "Load Dotfiles..."
 
 sed -i 's/\(root:.*\/root:\)\/bin\/ash/\1\/usr\/bin\/fish/g' /etc/passwd
 
-ln -s $(pwd)/fish/config.fish ~/.config/fish/config.fish
-ln -s $(pwd)/fish/functions ~/.config/fish/functions
+ln -f -s $(pwd)/fish/config.fish ~/.config/fish/config.fish
+ln -f -s $(pwd)/fish/functions ~/.config/fish/functions
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 mkdir -p ~/.vim/colors
-ln -s $(pwd)/vim/molokai.vim ~/.vim/colors/molokai.vim
-ln -s $(pwd)/vimrc ~/.vimrc
-ln -s $(pwd)/tmuxconf ~/.tmux.conf
+ln -f -s $(pwd)/vim/molokai.vim ~/.vim/colors/molokai.vim
+ln -f -s $(pwd)/vimrc ~/.vimrc
+ln -f -s $(pwd)/tmuxconf ~/.tmux.conf
 
 bash -c "command -v gem > /dev/null && gem install rails_best_practices"
 bash -c "command -v yarn > /dev/null && yarn global add prettier"
