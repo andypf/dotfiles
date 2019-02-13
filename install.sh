463db64def908e805b97aa2a3062e8591410f090
andypf <andreas.pfau@sap.com>
2019-02-13 09:04:14 +0000

    move vim and tmux dotfiles to persistent folder

--- install.sh
+++ install.sh
@@ -5,6 +5,15 @@ sed -i 's/\(root:.*\/root:\)\/bin\/ash/\1\/usr\/bin\/fish/g' /etc/passwd
 ln -s $(pwd)/fish/config.fish ~/.config/fish/config.fish
 ln -s $(pwd)/fish/functions ~/.config/fish/functions
 
+mkdir -p $(pwd)/vim
+mkdir -p $(pwd)/tmux
+
+ln -s $(pwd)/vim ~/.vim
+ln -s $(pwd)/tmux ~/.tmux
+
+ln -s $(pwd)/fish/functions ~/.config/fish/functions
+ln -s $(pwd)/fish/functions ~/.config/fish/functions
+
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm 
@@ -15,7 +24,6 @@ mkdir -p ~/.vim/colors
 ln -s $(pwd)/vimrc ~/.vimrc
 ln -s $(pwd)/tmuxconf ~/.tmux.conf
 
-bash -c "command -v pip3 > /dev/null && pip3 install --user pynvim"
 bash -c "command -v gem > /dev/null && gem install rubocop"
 bash -c "command -v yarn > /dev/null && yarn global add prettier"
 bash -c "command -v yarn > /dev/null && yarn global add eslint" 
