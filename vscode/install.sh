54142ab6de9c7eef4bde2eec93c8c670cef0ff9a
Andreas Pfau <ap@a-pfau.de>
2019-09-19 13:23:35 +0200

    Update install.sh

--- vscode/install.sh
+++ vscode/install.sh
@@ -5,3 +5,19 @@ echo "install extensions"
 #code-server --install-extension ms-vscode.go && \
 
 cp -r /root/dotfiles/vscode/user /root/.local/share/code-server/User
+
+sed -i 's/\(root:.*\/root:\)\/bin\/ash/\1\/usr\/bin\/fish/g' /etc/passwd
+
+ln -s /root/dotfiles/fish/config.fish ~/.config/fish/config.fish
+ln -s /root/dotfiles/fish/functions ~/.config/fish/functions
+
+mkdir -p /root/dotfiles/tmux
+
+ln -s /root/dotfiles/tmux ~/.tmux
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm 
+# git clone git://github.com/wting/autojump.git /tmp/autojump && python /tmp/autojump/install.py
+
+
+mkdir -p ~/.vim/colors
+ln -s /root/dotfiles/tmuxconf ~/.tmux.conf
