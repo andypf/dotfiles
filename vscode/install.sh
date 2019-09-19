9cf62f363cc443b90571cdb7634b862a41ff34d1
Andreas Pfau <ap@a-pfau.de>
2019-09-19 14:54:10 +0200

    Update install.sh

--- vscode/install.sh
+++ vscode/install.sh
@@ -4,20 +4,20 @@ echo "install extensions"
 
 #code-server --install-extension ms-vscode.go && \
 
-cp -r /root/dotfiles/vscode/user /root/.local/share/code-server/User
+cp -r $(pwd)/vscode/user /root/.local/share/code-server/User
 
 sed -i 's/\(root:.*\/root:\)\/bin\/ash/\1\/usr\/bin\/fish/g' /etc/passwd
 
-ln -s /root/dotfiles/fish/config.fish ~/.config/fish/config.fish
-ln -s /root/dotfiles/fish/functions ~/.config/fish/functions
+ln -s $(pwd)/fish/config.fish ~/.config/fish/config.fish
+ln -s $(pwd)/fish/functions ~/.config/fish/functions
 
-mkdir -p /root/dotfiles/tmux
+mkdir -p $(pwd)/tmux
 
-ln -s /root/dotfiles/tmux ~/.tmux
+ln -s $(pwd)/tmux ~/.tmux
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm 
 # git clone git://github.com/wting/autojump.git /tmp/autojump && python /tmp/autojump/install.py
 
 
 mkdir -p ~/.vim/colors
-ln -s /root/dotfiles/tmuxconf ~/.tmux.conf
+ln -s $(pwd)/tmuxconf ~/.tmux.conf
